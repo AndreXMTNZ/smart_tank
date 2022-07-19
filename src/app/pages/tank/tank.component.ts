4f199c164af96ea68d6732cb9cf8de5664d538e4
AndreXMTNZ <andres04m@outlook.com>
2022-07-18 22:42:57 -0600

    avances

--- src/app/pages/tank/tank.component.ts
+++ src/app/pages/tank/tank.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import { tick } from '@angular/core/testing';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-//import { start } from 'repl';
 
 @Component({
   selector: 'app-tank',
@@ -9,12 +7,31 @@ import { AngularFireDatabase } from '@angular/fire/compat/database';
   styleUrls: ['./tank.component.css'],
 })
 export class TankComponent implements OnInit {
-
+  //Principal
   seccion: 'auto' | 'manual' | 'test' = 'auto';
+  bot1: boolean = false;
+  bot2: boolean = false;
+  flt: boolean = false;
+  rgl: boolean = false;
+
+  //Visualizacion
   mediciones: string
   tds: string
   nivel:string 
   ph: string
+
+  //Manual
+  umbral: number = 25;
+  umbral2: number = 700;
+  umbral3: number = 7;
+
+  modTemp = 0;
+  modo: string = "auto";
+
+  lamp1: string
+  lamp2: string
+  ind: string
+  rgb: string
 
   constructor(public firedata: AngularFireDatabase) { 
     this.leerTemperatura();
@@ -60,7 +77,141 @@ export class TankComponent implements OnInit {
   segmentChanged(evento: any) {
     this.seccion = evento.detail.value;
     console.log('evento.detail.value ->', evento.detail.value);
+    
+    if (evento.detail.value == "auto"){
+      console.log("modo auto")
+      const path = 'modo';
+      this.firedata.object(path).set(this.modo);
+      this.modo = this.seccion;;
+      //this.firedata.object('modo').set(this.leerMods)
+    }
+
+    if (evento.detail.value == "manual"){
+      console.log("modo manual")
+      const path = 'modo';
+      this.firedata.object(path).set(this.modo);
+      this.modo = this.seccion;
+      //this.firedata.object('modo').set(this.leerMods)
+    }
+
+    if (evento.detail.value == "test"){
+      console.log("modo de prueba")
+      const path = 'modo';
+      this.firedata.object(path).set(this.modo);
+      this.modo = this.seccion;;
+      //this.firedata.object('modo').set(this.leerMods)
+    }
+    
   }
+
+  rangeChange1(evento: any) {
+    this.umbral = evento.detail.value;
+    const path = 'config/umbral1';
+    this.firedata.object(path).set(this.umbral);
+
+  }
+
+  rangeChange2(evento: any) {
+    this.umbral2 = evento.detail.value;
+    const path = 'config/umbral2';
+    this.firedata.object(path).set(this.umbral2);
+    
+  }
+
+  rangeChange3(evento: any) {
+    this.umbral3 = evento.detail.value;
+    const path = 'config/umbral3';
+    this.firedata.object(path).set(this.umbral3);
+
+  }
+
+  botlamp1(evento: any){
+    if(this.bot1){
+      this.bot1 = false;
+    }
+    else{
+      this.bot1 = true;
+    }
+    console.log(this.bot1);
+
+    if(this.bot1 == true){
+      this.lamp1 = "ON";
+    }
+
+    if(this.bot1 == false){
+      this.lamp1 = "OFF";
+    }
+  }
+
+  botlamp2(evento: any){
+    if(this.bot2){
+      this.bot2 = false;
+    }
+    else{
+      this.bot2 = true;
+    }
+    console.log(this.bot2);
+
+    if(this.bot2 == true){
+      this.lamp2 = "ON";
+    }
+
+    if(this.bot2 == false){
+      this.lamp2 = "OFF";
+    }
+  }
+  
+  fltint(evento: any){
+    if(this.flt){
+      this.flt = false;
+    }
+    else{
+      this.flt = true;
+    }
+    console.log(this.flt);
+
+    if(this.flt == true){
+      this.ind = "ON";
+    }
+
+    if(this.flt == false){
+      this.ind = "OFF";
+    }
+  }
+
+  servo(evento: any){
+    if(this.rgl){
+      this.rgl = false;
+    }
+    else{
+      this.rgl = true;
+    }
+    console.log(this.rgl);
+
+    if(this.rgl == true){
+      this.rgb = "ON";
+    }
+
+    if(this.rgl == false){
+      this.rgb = "OFF";
+    }
+  }
+
+  leerMods() {
+    const path = 'valores/temp';
+    this.firedata.object(path).set(0);
+  }
+/*
+  lamp1Change(evento: any) {
+    this.lamp1 = evento.detail.value;
+    const path = 'lamp1';
+    //if (this.lamp1Change == 'on'){
+
+    //}
+    this.firedata.object(path).set(this.umbral);
+    this.lamp1 = this.lamp1;
+    this.firedata.object('modo/lamp1').set(this.lamp1Change)
+  }*/
 
 }
 
